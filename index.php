9baaa0eaaa2254454b6c4ad4fa855dc4028cdb7d
hamyu <hamyu_kimurin@hotmail.com>
2017-07-30 12:04:44 +0900

    php transfer

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
-<?php phpinfo(); ?>
+<?php
+$json_string = file_get_contents('php://input');
+
+$curl = curl_init("https://153.188.229.218/index.php");
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_CUSTOMREQUEST, 'POST');
+curl_setopt($curl, CURLOPT_POSTFIELDS, $json_string);
+curl_setopt($curl, CURLOPT_HTTPHEADER, array('Content-Type: application/json; charser=UTF-8'));
+curl_exec($curl);
+curl_close($curl);
+?>
